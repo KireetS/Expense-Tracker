22cf65982a4b791a78513eef271c0c432a86eac3
Kireet <singhkireet5@gmail.com>
2023-09-04 15:53:48 +0530

    made secrets

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
+const MongoUrl = process.env.MONGODB_URI
 
 async function connectToMongo() {
   try{
-    await mongoose.connect('mongodb://127.0.0.1:27017/FitnessTracker');
+    await mongoose.connect(MongoUrl ,{ useNewUrlParser: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("connected To Mongo now")
   }catch(err){
     console.log("error in connection with mongo",err)
