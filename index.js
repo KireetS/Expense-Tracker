aded22225d4df08df6ee2b667eef6e7dffb3933f
Kireet <singhkireet5@gmail.com>
2023-09-05 10:41:22 +0530

    trying for render deploy

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ connectToMongo()
 
 const app = express()
 const port = process.env.PORT||5000
-const corsOptions = {
-  origin: "https://expensetrackerbackend-jpv8at9jk-kireets.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json())
 app.use("/api/auth" , require("./routes/auth"))
