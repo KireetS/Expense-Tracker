efa45ed8116bd4755673fa3ea87d54643fc40eee
Kireet <singhkireet5@gmail.com>
2023-09-05 10:29:05 +0530

    try1

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ connectToMongo()
 
 const app = express()
 const port = process.env.PORT||5000
-app.use(cors({
-  origin:"https://expensetrackerbackend-jpv8at9jk-kireets.vercel.app/"
-}))
+const corsOptions = {
+  origin: "https://expensetrackerbackend-jpv8at9jk-kireets.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use("/api/auth" , require("./routes/auth"))
